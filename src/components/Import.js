36da79abcf3acd951cf32a3eb729c5c909a4cc65
UlyPanda <ulypanda44@gmail.com>
2021-01-15 01:45:10 -0600

    Completed!

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Container, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 
 const Import = (props) => {
-    // fill out this component
+        const [makeId, setMakeId] = useState(null);
+
+        const [anchorEl, setAnchorEl] = useState(null);
+      
+        const handleClick = (event, index) => {
+          setAnchorEl(event.currentTarget);
+          setMakeId(index);
+        };
+      
+        const handleClose = () => {
+          setAnchorEl(null);
+          setMakeId(null);
+        };
+      
 
     return (
-        <p>Import Component</p>
-    )
+    <Container maxWidth="md" className='import-container' >
+            <h2> COUNT: {props.makes.length} </h2>
+        <Button  onClick={props.fetchMakes} variant='contained' color='primary'>Import</Button>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell align='right'>Make</TableCell>
+                    <TableCell align='right'>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+                <TableBody>
+                    {props.makes.map((make, idx) => (
+                        <TableRow key={idx}>
+                            <TableCell scope='make'>{make["MakeId"]}</TableCell>
+                            <TableCell align='right'>{make["MakeName"]}</TableCell>
+                            <TableCell align='right'>
+                                <Button aria-controls='simple-menu' aria-haspopup='true' onClick={ (event) => handleClick(event, idx) } >
+                                    <MoreVertIcon/>
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+        </Table>
+
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem 
+              onClick={() => {props.deleteMake(makeId) 
+              handleClose() 
+              }}>
+              Delete
+              </MenuItem>
+            </Menu>
+         
+
+    </Container>
+
+    )  
 }
 
 export default Import
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,9 +7,30 @@ export const addCar = (car) => {
     }
 }
 
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
+        .then(res => res.json())
+        .then(response => {
+            const action = {
+                type: 'FETCH_MAKES',
+                value: response.Results
+            }
+            dispatch(action)
+        })
+    }
+}
+
 export const removeCar = (index) => {
     return {
         type: 'REMOVE_CAR',
+        value: index
+    }
+}
+
+export const deleteMake = (index) => {
+    return {
+        type: 'DELETE_MAKE',
         value: index
     }
 }
